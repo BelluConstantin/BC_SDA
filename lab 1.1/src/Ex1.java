1aa436e156e3f99eb9d4fbac60244c8ee2e0e20a
BelluConstantin <k.bellos7@yahoo.com>
2020-03-18 16:57:37 +0200

    Ex1

---
+++ lab 1.1/src/Ex1.java
@@ -0,0 +1,37 @@
+//package ro.unitbv.javadatatype;
+
+import java.util.Scanner;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		//System.out.println("test");
+	
+	    Scanner scanner = new Scanner(System.in);
+		
+	    System.out.println("Introduce primul numar : ");
+	    int nr1 = scanner.nextInt();				
+	    System.out.println("Numarul citit este: "+ nr1);
+	    
+	    System.out.println("Introduce al doilea numar: ");
+	    int nr2 = scanner.nextInt();
+	    int suma = nr1 + nr2;
+	    int produs = nr1 * nr2;
+	    double media = (nr1 + nr2) / 2;
+	    double impartire = (double)nr1 / nr2;
+	    int distanta;
+		if (nr1 > nr2)
+	    	distanta = nr1 - nr2;
+	    else 
+	    	distanta = nr2 - nr1;
+	    System.out.println("nr citit este: "+ nr2);
+	    System.out.println("------------------------");
+	    System.out.println("Suma numerelor este: "+ suma);
+	    System.out.println("Produsul este: " + produs);
+	    System.out.println("Media este: " + media);
+	    System.out.println("Distanta este: " + distanta);
+	    System.out.println("Maximul celor doua numere este: "+ Math.max(nr1,  nr2));
+	    System.out.println("" + impartire);
+
+}
+
+}
